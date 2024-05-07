b86d3386729159f7f6c2e6aef094053580d80765
birdcross <birdcross10@gmail.com>
2024-05-07 09:22:52 +0900

    copy 04

--- src/main/java/com/green/PrjJpa05Application.java
+++ src/main/java/com/green/PrjJpa05Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PrjJpa03Application {
+public class PrjJpa05Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PrjJpa03Application.class, args);
+		SpringApplication.run(PrjJpa05Application.class, args);
 	}
 
 }
